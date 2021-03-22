dc09be3f81711369c3711da340efc26602c5e91d
arcturus5340 <arcturus5340@gmail.com>
2021-03-22 19:05:53 +0100

    Add support for api/users POST requests

--- src/index.js
+++ src/index.js
@@ -26,6 +26,25 @@ class JuliaAPI {
             console.log(error);
         })
     }
+
+    createUser(user) {
+        return axios.post(`${this.protocol}://${this.address}/api/users/`, user, this.config)
+        .then( response => response.data )
+        .catch( error => {
+            error = error.response;
+            if (error.data){
+                if (error.data.username) {
+                    throw `Invalid credentials: ${error.data.username}`;
+                }else if (error.data.password) {
+                    throw `Invalid credentials: ${error.data.password}`;
+                }else if (error.data.email){
+                    throw `Invalid credentials: ${error.data.email}`;
+             }
+            }else{
+                throw `Invalid credentials: A user with that username already exists.`;
+            }
+        })
+    }
 }
 
 export {JuliaAPI};
