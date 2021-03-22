1eac1220d324e6e74c4a424c8a349b39bb79b471
arcturus5340 <arcturus5340@gmail.com>
2021-03-22 19:05:29 +0100

    Add support for api/users GET requests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+import axios from "axios";
+
+
 class JuliaAPI {
-    constructor(address) {
+    constructor(address = "localhost:8000", secure = true) {
         this.address = address;
+        if(secure){
+            this.protocol = 'https';
+        }else{
+            this.protocol = 'http';
+        }
+        this.config = {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        }
+    }
+
+    listUsers(params) {
+        let config_copy = {...this.config};
+        config_copy["params"] = params;
+        return axios.get(`${this.protocol}://${this.address}/api/users/`, config_copy)
+        .then( response => response.data )
+        .catch( error => {
+            console.log(error);
+        })
     }
 }
+
+export {JuliaAPI};
